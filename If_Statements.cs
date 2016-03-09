bd3ced6ca219c5ebab863071182096977954cb14
Amanda <Faylinn@users.noreply.github.com>
2016-03-08 18:27:24 -0700

    Thank you.

---
+++ If_Statements.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour 
+{
+	public float score = 60f;
+
+	void Start()
+	{
+		if (80f <= score) 
+		{
+			Debug.Log ("Awesome!");
+		}
+		else if (70f == score)
+		{
+			Debug.Log ("Almost Awesome");
+		}
+		else
+		{
+			Debug.Log ("Not Awesome");
+		}
+	}
+}
