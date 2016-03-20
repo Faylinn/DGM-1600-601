208fdb31ab16132c88ba59c74555f34918b46878
Faylinn <amanda.marie.steel@gmail.com>
2016-03-19 20:48:07 -0600

    Cleaned up the repository

---
+++ Scripts/Loops.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour 
+{
+	void Start()
+	{
+		int cat = 4;
+		
+		while(cat > 0)
+		{
+			Debug.Log ("missing cats");
+			cat--;
+		}
+		
+		int dog = 3;
+		
+		do
+		{
+			Debug.Log("missing dogs");
+		}while (dog ==7);
+		
+		int bird = 2;
+		
+		for(int x = 0; x < bird; x++)
+		{
+			Debug.Log ("missing birds");
+		}
+		
+		int[] fish = new int[3];
+		fish[0] = 6;
+		fish[1] = 7;
+		fish[2] = 2;
+		
+		foreach(int bubbles in fish)
+		{
+			Debug.Log(bubbles);
+		}
+	} 
+}
