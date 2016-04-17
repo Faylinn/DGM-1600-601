f984d82698256b0a5da134709099f74acd658c96
Faylinn <amanda.marie.steel@gmail.com>
2016-04-16 23:56:13 -0600

    Change MIdterm to Final and finished final scripts

---
+++ FinalProject/Assets/EnemyDamage.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnemyDamage : MonoBehaviour
+{
+    /*
+     * The OnCollisionEnter function:
+     * If the tag of the Collision parameter equals the KillZone tag then
+     *      Destroy this GameObject
+     */
+
+    /* **************Explanation and Hints************
+     * OnCollisionEnter - This is a special Unity function. It runs whenever a GameObject with rigidbody attached to it
+     *                    runs into another object. In this script we are using it to detect when the enemy runs into
+     *                    the kill zone. One thing this function needs is a Collision parameter. The collision
+     *                    parameter is useful because it gives us the reference of whatever the enemy hits. The function
+     *                    is typed like this: 
+     *                    void OnCollisionEnter(Collision *name*)
+     *                    {
+     *                        //Run code here
+     *                    }
+     *                    Replce *name* with what you want to call the Collision variable
+     * Tag - Tags are used to identify GameObjects. In the inspector we can create new tags and set tags for GameObjects.
+     *       In this project we will want to create two tags. First one is "Enemy" and the second one is "KillZone". Then
+     *       we want to give the enemies the "Enemy" tag, give the player the "Player" tag, and give the KillZone the
+     *       "KillZone" tag.
+     *     - In the OnCollisionEnter function we will type If statements. The conditionals of these if statements will be
+     *       comparing the passed in Collision variable's tag with the strings "Enemy" or the KillZone. We do this so
+     *       we know what the player has hit. You will type out these If statements in the OnCollisionEnter function
+     *       like this:
+     *       If(*collisionVariable*.transform.tag == *stringOfTag*)
+     *       {
+     *          //Do stuff when this hits an GameObject with the tag specified above
+     *       }
+     *       Replace *collisionVariable* with the name that you have given the Collision variable. Replace *stringOfTag*
+     *       with either the "Enemy" tag or the "KillZone" tag.
+     *       
+     * Destroy - This function will destroy whatever GameObject we pass in as a parameter. For example we
+     *           can type:
+     *           Destroy(gameObject);
+     *           This will destroy the GameObject that this script is attached to. For our project we want
+     *           to use this along with the timer so that the enemies that have spawned don't stick around
+     *           forever.
+     */
+
+/*************************************************************************************************************************/
+
+	/*
+	* Additional Variables:
+	* A public enum that will contain constants for three different types of enemys    
+	* A public variable with the type being the enum above. This will determine what type of enemy this one is. 
+	* A public int for the enemy's health 
+	*/
+
+	/* 
+	* A damage function, void return type, parameters: an int for damage amount 
+	*       Have the health "subtract equals" the parameter that was passed in.  
+	*       if(the health variable is less than 0)
+	*           Destroy this enemy
+	*/
+
+	/* **************Explanation and Hints************   
+	* Subtract equals - This will take a variable and subtract a certain amount from it. For example: 
+	*                            
+	*                   *healthVariable* -= *damageAmount*  
+	*                           
+	*                   So if the health variable equals 15 and damage amount equal five then after running this line of    
+	*                   code the health variable will now equal 10    
+	* Destroy this game object - When the enemy has lost all of it's health we want it to be destroyed. We can do this by typing the following:  
+	*                              
+	*                            Destroy(gameObject);   
+	*/
+	
+	public enum Enemies {SQUARE, SPHERE, CYLINDER};
+	public Enemies enemyType;
+	public int health;
+
+	void OnCollisionEnter(Collision col)
+	{
+		//if (col.transform.tag == "Enemy" || col.transform.tag == "KillZone")
+		if (col.transform.tag == "KillZone")
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	public void Damage(int amount)
+	{
+		health -= amount;
+
+		if (health <= 0) 
+		{
+			Destroy (gameObject);
+		}
+
+	}
+}
---
+++ FinalProject/Assets/EnemyMovement.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    /* 
+     * Variables:
+     * A private Rigidbody variable
+     * A public float for the speed
+     * A public float for the direction
+     */
+
+    /*
+     * The Start function:
+     * Use GetComponent to get the rigidbody reference
+     * Create a new Vector3 value for the direction variable with X and Z being zero and Y being a Random number between 0 and 360.
+     * Assign the direction to the eulerAngles of this GameObject in order to rotate this GameObject
+     */
+
+	
+    /*
+     * The Update function:
+     * Using the Rigidbody variable use AddRelativeForce and pass the forward direction times the speed as parameters
+     */
+
+
+    /* **************Explanation and Hints************
+     * Rigidbody - When attached to a GameObject it adds things like gravity as well as other physics properties to the GameObject.
+     *             For the player we want to attach the Rigidbody to it so that we can add a forward force to it when pressing the up arrow key
+     *           - To use the Rigidbody we need a variable. The type of this variable is "Rigidbody" instead of "int" and often this variable
+     *             is named "rigidbody".
+     *           - Now we need to get the reference to the Rigidbody. If we don't then it won't work. In order to get the reference we need to
+     *             type " *nameOfRigidbodyVariable* = GetComponentComponent<Rigidbody>(); " in the Start function. Replace *nameOfRigidbodyVariable*
+     *             with whatever name you gave the rigidbody variable.
+     *             
+     * Vector3 - It's a variable that groups "x", "y", and "z" together. In this script we will be assigning a new Vector3 value with the random direction
+     *           We'll do this by typing:
+     *           *directionVariable* = new Vector(0, Random.Range(0, 360), 0);
+     *           After we do with we'll want to assign this to the eulerAngles.
+     *           
+     * EulerAngles - There are two way to represent rotation in Unity. The first one is Quaternion and the second is EulerAngles. In this script we
+     *               will be using the eulerAngles that's in transform since eulerAngles are easier to understand. Basically a eulerAngle is a
+     *               Vector3 where the values of x, y, and z go from 0 to 360. So in this case if y has a value of 180 then this GameObject will
+     *               be rotated 180 degrees on the y axis. In other words it will turn around. To assign a value to eulerAngles we'll type:
+     *               transform.eulerAngles = *directionVariable*
+     *               Replace *directionVariable* with the Vector3 direction variable that has a random value for the y axis.
+     *              
+     * AddRelativeForce - This will add a force relative to the direction of this GameObject. It is also a part of Rigidbody. In this script
+     *                    we want to add force to the forward direction of this GameObject times the speed variable;
+     *                  - We'll do this by typing:
+     *                    *rigidbodyVariable*.AddRelativeForce(transform.forward * speed);
+     */
+
+/*************************************************************************************************************************/
+
+	/*
+	* Additional Variables:
+	* A private GameObject to hold a reference to the player
+	* A private variable with the type being your Enemy Damage Script
+	*/
+
+	/*
+	* Additional Lines in the Start Function:
+	*      Use FindGameObjectWithTag with the player's tag in order to get the reference to the player - place this line above the ones that were there before
+	*      Use GetComponent to get the reference to your enemy damage script - place this line above the ones that were there before
+	*      If (the type variable in the enemy damage script is equal to the first enum constant in the enemy damage script) - place this if statement at the bottom of the function
+	*          Move the line that sets a random direction into this If statement
+	*/
+
+	/*
+	* Additional Lines in the Update Function:
+	*      If (the type variable in the enemy damage script is equal to the second enum constant OR the third enum constant in the enemy damage script)
+	*          Use the LookAt function with the player's transform.
+	*/
+
+	/* **************Explanation and Hints************ 
+	*  Damage Script variable type - When we want to use functions and variables from another script we need to have a variable that uses the script
+	*                                as the type. In this script we want to use the enemy damage script. So we would write it like this:
+	*                                
+	*                                private *enemyDamageScriptName* *enemyDamageVariableName*;
+	*                                
+	*                                Replace *enemyDamageScriptName* with the name of your enemy damage script. Replace *enemyDamageVariableName* with a name of 
+	*                                your choosing. Please look at the Classes and Datatypes assignment for more details.
+	*                                
+	* Accessing variables in the enemy damage script - To do this we need to use the dot operator. Please look at the Scope and Access Modifier assignment as well
+	*                                                  as the Classes and Datatypes assignment. 
+	* 
+	* FindGameObjectWithTag - This will find a GameObject with a specific tag. In this case we are using it to get a reference to the player.
+	*                         You can do this by typing out the following
+	*
+	*                         *nameOfPlayerVariable* = GameObject.FindGameObjectWithTag("EnterPlayerTagHere");
+	*                         
+	*  GetComponent - This allows us to get a reference to a script that's attached to the same GameObject that this script is attached to. In this
+	*                  script we want to get a reference to the enemy damage script. This can be done by typing the following:
+	*                  
+	*                  *enemyDamageVariableName* = GetComponent<*enemyDamageScriptName*>();
+	*                  
+	*                  Replace *enemyDamageScriptName* with the name of your enemy damage script. Replace *enemyDamageVariableName* with a name of 
+	*                  your choosing. 
+	*
+	* Getting a reference to the enemy damage script - You can do this the same way as getting the rigidbody except you replace "rigidbody" for
+	*                                                  the name of the script. For example:
+	*                                                   
+	*                                                  *enemyDamageVariableName* = GetComponent<*enemyDamageScriptName*>();
+	*
+	* Comparing the type of enemy - In order to figure out what type of enemy this is we need to use the enemy damage variable from above.
+	*                               Then we use the dot operator to access the type variable in that script. After the double equals we need
+	*                               to use the enemy damage variable once again to access the enum we are comparing the type to.
+	*
+	* LookAt - The LookAt function rotates this GameObject to face a target. In this case we want to rotate the enemy so that it is facing the player.
+	*          We can do this by typing the following:
+	*          
+	*          transform.LookAt(*nameOfPlayerVariable*.transform);
+	*/
+
+	private Rigidbody rigidbody;
+	public float speed;
+	public Vector3 dir;
+	private GameObject player;
+	private EnemyDamage dmg;
+
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+		dmg = GetComponent<EnemyDamage> ();
+		rigidbody = gameObject.GetComponent<Rigidbody> ();
+
+		if (dmg.enemyType == EnemyDamage.Enemies.SQUARE) 
+		{
+			dir = new Vector3 (0, Random.Range (0, 360), 0);
+		}
+
+		transform.eulerAngles = dir;
+
+
+	}
+
+	void Update()
+	{
+		rigidbody.AddRelativeForce (transform.forward * speed);
+
+		if (dmg.enemyType == EnemyDamage.Enemies.SPHERE || dmg.enemyType == EnemyDamage.Enemies.CYLINDER)
+		{
+			transform.LookAt (player.transform);
+		}
+	}
+}
