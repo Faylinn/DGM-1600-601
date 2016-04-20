08893f089273f465a359c77d8503f1e1eaee0af3
Faylinn <amanda.marie.steel@gmail.com>
2016-04-19 21:17:39 -0600

    Final project

---
+++ Scripts/SwitchStatements.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchStatements : MonoBehaviour 
+{
+	public int cats = 3;
+	
+	void Start()
+	{
+		switch(cats)
+		{
+			case 3:
+				print ("Too many cats");
+				break;
+			case 2:
+				print ("Great amount of cats");
+				break;
+			case 1:
+				print ("Your cat needs a friend");
+				break;
+			case 0:
+				print ("Do you need a cat?");
+				break;
+			default:
+				print ("You crazy cat person!");
+				break;
+		}
+	}
+}
