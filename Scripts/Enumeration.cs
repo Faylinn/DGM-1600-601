981c7381266b0713ddbb1031ecc9d87a7063592e
Faylinn <amanda.marie.steel@gmail.com>
2016-04-27 22:37:07 -0600

    Enum

---
+++ Scripts/Enumeration.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enumeration : MonoBehaviour
+{
+	enum animal{dog,cat, bird, fish};
+	
+	void Start()
+	{
+		animal myAnimal;
+		myAnimal = animal.dog;
+		myAnimal = changeAnimal (myAnimal);
+		print (myAnimal);
+	}
+	
+	animal changeAnimal(animal ani)
+	{
+		if(ani == animal.dog)
+			ani = animal.cat;
+		else if (ani == animal.cat)
+			ani = animal.dog;
+		return ani;
+	}
+}
